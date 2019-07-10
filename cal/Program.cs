e8a58336a77fce7e29cd8b092dc5ec08d0f6ea89
รังสรรค์ สังข์นาค <support_beach@hotmail.com>
2019-07-10 13:15:51 +0700

    init

---
+++ cal/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cal
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input Num1 : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input Operation (+),(-),(*),(/)  : ");
+            string oper = Console.ReadLine();
+
+
+            Console.Write("Input Num2 : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+                    switch (oper) 
+                {
+                      case "+" :
+                           
+                          Console.WriteLine(num1+num2);
+                    Console.Read();
+
+                          break;
+                      case "-":
+                    Console.WriteLine(num1 - num2);
+                    Console.Read();
+                    break;
+                      case "*":
+                    Console.WriteLine(num1 * num2);
+                    Console.Read();
+                    break;
+                      case "/":
+                    Console.WriteLine(num1 / num2);
+                    Console.Read();
+                    break;
+                      default:
+                        Console.WriteLine("Input Again");
+                        break;
+                }
+
+
+
+
+        }
+    }
+}
